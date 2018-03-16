bb636d650fc709e70906486392c0097904e2fb78
waffoo <mukuwaffle@gmail.com>
2018-03-16 16:17:42 +0900

    add knapsack

--- ants/chap2/knapsack.cpp
+++ ants/chap2/knapsack.cpp
@@ -1,8 +1,6 @@
-#include <algorithm>
+// p52 knapsack-dp
 #include <climits>
 #include <iostream>
-#include <queue>
-#include <utility>
 #include <vector>
 using namespace std;
 
@@ -18,9 +16,41 @@ template <typename Head, typename... Args>
 void print(Head head, Args... args);
 const int INF = INT_MAX / 2;
 
+struct Obj {
+    Obj(int w, int v) : weight(w), value(v) {}
+    int weight;
+    int value;
+};
+
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(false);
+
+    int n, lim, w, v;
+    vector<Obj> goods = {Obj(0, 0)};
+    vector<vector<int>> dp;
+    cin >> n;
+    rep(i, n) {
+        cin >> w >> v;
+        goods.pb(Obj(w, v));
+    }
+    cin >> lim;
+    rep(i, n + 1) dp.pb(vector<int>(lim + 1, 7));
+
+    repall(row, dp) row[0] = 0;
+    repall(i, dp[0]) i = 0;
+
+    FOR(i, 1, n + 1) {
+        FOR(j, 1, lim + 1) {
+            if (goods[i].weight > j)
+                dp[i][j] = dp[i - 1][j];
+            else
+                dp[i][j] = max(dp[i - 1][j],
+                               dp[i - 1][j - goods[i].weight] + goods[i].value);
+        }
+    }
+
+    print(dp[n][lim]);
 }
 
 //---------------------------------------------
@@ -31,6 +61,6 @@ void print(Arg arg) {
 
 template <typename Head, typename... Args>
 void print(Head head, Args... args) {
-    cout << head << " ";
+    cout << head << ' ';
     print(args...);
 }
