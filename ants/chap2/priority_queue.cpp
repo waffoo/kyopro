a629ea5f4fc3c8bb79cd2e2690a0d9e2129f2699
waffoo <mukuwaffle@gmail.com>
2018-03-23 00:12:27 +0900

    add priority_queue.cpp

--- ants/chap2/priority_queue.cpp
+++ ants/chap2/priority_queue.cpp
@@ -1,3 +1,4 @@
+// p73 expedition
 #include <algorithm>
 #include <climits>
 #include <iostream>
@@ -8,7 +9,7 @@
 using namespace std;
 
 #define FOR(i, a, b) for (int i = (a); i < static_cast<int>(b); i++)
-#define FORR(i, a, b) for (int i = (a); i >= static_cast<int>(b); i--)
+#define FORR(i, a, b) for (int i = (a); i > static_cast<int>(b); i--)
 #define rep(i, n) FOR (i, 0, n)
 #define repall(i, arr) for (auto& i : (arr))
 #define all(x) (x).begin(), (x).end()
@@ -31,6 +32,31 @@ const int INF = INT_MAX / 2;
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(false);
+
+    int n, l, p, ta, tb;
+    int gas[1000000] = {0};
+    pq<int> q;
+
+    cin >> n >> l >> p;
+    q.push(p);
+    rep (i, n) {
+        cin >> ta >> tb;
+        gas[ta] = tb;
+    }
+
+    int cnt = -1;
+    for (int pos = 0, tank = 0; pos < l; cnt++) {
+        tank = q.top(), q.pop();
+        while (tank > 0) {
+            tank--, pos++;
+            if (gas[pos] > 0) q.push(gas[pos]);
+        }
+        if (q.empty()) {
+            print(-1);
+            return 0;
+        }
+    }
+    print(cnt);
 
     return 0;
 }
