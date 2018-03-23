74e35cc15881056e365d1cff4351f16a539a730f
waffoo <mukuwaffle@gmail.com>
2018-03-23 22:31:26 +0900

    add bellman-ford

---
+++ ants/chap2/bellman_ford.cpp
@@ -0,0 +1,90 @@
+// p95 bellman-ford
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+#define FOR(i, a, b) for (int i = (a); i < static_cast<int>(b); i++)
+#define FORR(i, a, b) for (int i = (a); i >= static_cast<int>(b); i--)
+#define rep(i, n) FOR (i, 0, n)
+#define repall(i, arr) for (auto& i : (arr))
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define dump(x) cerr << #x << " = " << (x) << '\n'
+template <typename T>
+void print(const vector<vector<T>> matrix);
+template <typename T>
+void print(const vector<T> vec);
+template <typename Arg>
+void print(const Arg arg);
+template <typename Head, typename... Args>
+void print(const Head head, const Args... args);
+template <typename T>
+using pq = priority_queue<T>;
+template <typename T>
+using pqr = priority_queue<T, vector<T>, greater<T>>;
+const int INF = INT_MAX / 2;
+
+struct Edge {
+    int from, to, cost;
+};
+
+int node(char c) { return c - 'A'; }
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    vector<Edge> edges;
+    vector<int> dis(10, INF);
+    char start, goal, from, to;
+    int cost;
+    cin >> start >> goal;
+    dis[node(start)] = 0;
+
+    while (cin >> from >> to >> cost) {
+        edges.pb({node(from), node(to), cost});
+        edges.pb({node(to), node(from), cost});
+    }
+
+    while (true) {
+        bool update = false;
+        repall (e, edges) {
+            if (dis[e.from] != INF and dis[e.to] > dis[e.from] + e.cost) {
+                dis[e.to] = dis[e.from] + e.cost;
+                update = true;
+            }
+        }
+        if (not update) break;
+    }
+
+    print(dis[node(goal)]);
+
+    return 0;
+}
+
+//---------------------------------------------
+template <typename T>
+void print(const vector<vector<T>> matrix) {
+    repall (vec, matrix)
+        print(vec);
+}
+template <typename T>
+void print(const vector<T> vec) {
+    repall (i, vec)
+        cout << i << ' ';
+    cout << '\n';
+}
+template <typename Arg>
+void print(const Arg arg) {
+    cout << arg << '\n';
+}
+template <typename Head, typename... Args>
+void print(const Head head, const Args... args) {
+    cout << head << " ";
+    print(args...);
+}
