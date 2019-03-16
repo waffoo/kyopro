71c09e3a3546921451bf602aac173d0e00e0b631
waffoo <mukuwaffle@gmail.com>
2019-03-16 16:55:47 +0900

    add BIT.h

---
+++ util/binary_indexed_tree.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+
+class BIT {
+    // ATTENTION: this tree is one-indexed
+    std::vector<int> tree;
+    int MAX_SIZE;
+
+public:
+    BIT(int max) : tree(max), MAX_SIZE(max) {}
+    void add(int idx, int val) {
+        for (int x = a; x < MAX_SIZE; x += x & -x) tree[x] += w;
+    }
+    int sum(int idx) {
+        int ret = 0;
+        for (int x = a; x > 0; x -= x & -x) ret += tree[x];
+        return ret;
+    }
+}
