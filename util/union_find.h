440758cd4f82081fa1ab208e12f819b0ad1e78eb
waffoo <mukuwaffle@gmail.com>
2019-05-04 04:56:29 +0900

    move inpl of union-find to header file

--- util/union_find.h
+++ util/union_find.h
@@ -11,7 +11,16 @@ class UnionFind {
 
 public:
     UnionFind(int max) : parent(max + 1, -1), sz(max + 1, 1) {}
-    void unite(int x, int y);
+
+    void unite(int x, int y) {
+        x = root(x), y = root(y);
+        if (x != y) {
+            if (parent.at(x) > parent.at(y)) std::swap(x, y);
+            if (parent.at(x) == parent.at(y)) parent.at(x)--;
+            sz[x] += sz[y];
+            parent.at(y) = x;
+        }
+    }
     int size(int x) { return sz.at(root(x)); }
     bool isFamily(int x, int y) { return root(x) == root(y); }
 };
