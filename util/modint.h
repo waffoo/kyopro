3d2ed35b5405b1e02afcea514fe567f161aa769e
waffoo <mukuwaffle@gmail.com>
2019-07-21 03:49:55 +0900

    bugfix in modint

--- util/modint.h
+++ util/modint.h
@@ -7,21 +7,21 @@ class mint {
 
 public:
     mint(ll x) : num(x) {}
-    const mint& operator+=(const mint& x) noexcept {
+    mint operator+=(const mint& x) noexcept {
         this->num += x.num;
         if (this->num >= MOD) this->num %= MOD;
         return *this;
     }
-    const mint& operator-=(const mint& x) noexcept {
+    mint operator-=(const mint& x) noexcept {
         if (this->num < x.num) this->num += MOD;
         this->num -= x.num;
         return *this;
     }
-    const mint& operator*=(const mint& x) noexcept {
+    mint operator*=(const mint& x) noexcept {
         this->num = (this->num * x.num) % MOD;
         return *this;
     }
-    const mint& operator/=(mint x) noexcept {
+    mint operator/=(mint x) noexcept {
         ll exp = MOD - 2;
         while (exp) {
             if (exp % 2) *this *= x;
@@ -30,18 +30,10 @@ public:
         }
         return *this;
     }
-    const mint& operator+(const mint& x) const noexcept {
-        return mint(*this) += x;
-    }
-    const mint& operator-(const mint& x) const noexcept {
-        return mint(*this) -= x;
-    }
-    const mint& operator*(const mint& x) const noexcept {
-        return mint(*this) *= x;
-    }
-    const mint& operator/(const mint& x) const noexcept {
-        return mint(*this) /= x;
-    }
+    mint operator+(const mint& x) const noexcept { return mint(*this) += x; }
+    mint operator-(const mint& x) const noexcept { return mint(*this) -= x; }
+    mint operator*(const mint& x) const noexcept { return mint(*this) *= x; }
+    mint operator/(const mint& x) const noexcept { return mint(*this) /= x; }
 
     friend ostream& operator<<(ostream&, const mint&);
 };
